a73cadc4c245b11287b301c25d4b49f002155784
Fengjian Jiang <116087936+fengjianjiang41@users.noreply.github.com>
2025-08-25 18:23:37 +0800

    Create HJ1 字符串最后一个单词的长度.py

---
+++ HJ1 字符串最后一个单词的长度.py
@@ -0,0 +1,8 @@
+input_str=input()
+word=input_str.strip().split(' ')
+print(len(word[-1]))
+# input_str = input() 
+# word_list = input_str.strip().split() 
+# last_word = word_list[-1] 
+# last_word_len = len(last_word) 
+# print(last_word_len)
