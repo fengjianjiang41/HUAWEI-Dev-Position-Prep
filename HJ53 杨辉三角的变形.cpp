bdc245ac3e01d4d47fa7019a7d4e7f283c7b5aa1
Fengjian Jiang <116087936+fengjianjiang41@users.noreply.github.com>
2025-10-03 23:50:12 +0800

    Create HJ53 杨辉三角的变形.cpp

---
+++ HJ53 杨辉三角的变形.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin >> n)
+    {
+        if(n==1 || n==2)
+        {
+            cout << -1 << endl;
+        }
+        else if(n%4 == 3 || n%4 == 1)
+        {
+            cout << 2 << endl;
+        }
+        else if(n%4 == 0)
+        {
+            cout << 3 << endl;
+        }
+        else
+        {
+            cout << 4 << endl;
+        }
+    }
+    return 0;
+}
