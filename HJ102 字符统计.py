5314719bac77264e55a61ee749d248c30eef8fe5
Fengjian Jiang <116087936+fengjianjiang41@users.noreply.github.com>
2025-10-23 00:13:31 +0800

    Implement character counting and sorting
    Add character counting functionality that sorts characters by frequency and then alphabetically.

---
+++ HJ102 字符统计.py
@@ -0,0 +1,18 @@
+while True:
+    try:
+        list1 = []
+        arr = input()
+        dic = {}
+        for i in arr:
+            if not (i.isalpha() or i.isdigit() or i.isspace()):
+                continue
+            else:
+                if i in dic:
+                    dic[i] += 1
+                else:
+                    dic[i] = 1
+        dic = sorted(dic.items(), key=lambda x: x[0])  # 先按字符ASC排
+        dic = sorted(dic, key=lambda x: x[1], reverse=True)  # 再按统计数目排
+        print(''.join(k for (k, v) in dic))
+    except:
+        break
