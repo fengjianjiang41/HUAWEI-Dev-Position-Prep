224c6d13db0d2c8859cf03af3f233aa90f647795
Fengjian Jiang <116087936+fengjianjiang41@users.noreply.github.com>
2025-12-23 19:51:17 +0800

    Create HJ27 查找兄弟单词.py

---
+++ HJ27 查找兄弟单词.py
@@ -0,0 +1,18 @@
+import sys
+
+while True:
+    try:
+        ss = sys.stdin.readline().split()
+        n = int(ss[0])
+        dict = ss[1 : n + 1]
+        s = ss[-2]
+        m = int(ss[-1])
+        a = []
+        for i in dict:
+            if len(i) == len(s) and i != s and sorted(i) == sorted(s):
+                a.append(i)
+        print(len(a))
+        if a and m <= len(a):
+            print(sorted(a)[m - 1])
+    except:
+        break
