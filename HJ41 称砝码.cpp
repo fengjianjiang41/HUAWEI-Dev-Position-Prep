f5b2d5569ebe04ec722df7444ac8aa0bd2ef5ea3
Fengjian Jiang <116087936+fengjianjiang41@users.noreply.github.com>
2026-01-09 20:00:14 +0800

    Update HJ41 称砝码.cpp

--- HJ41 称砝码.cpp
+++ HJ41 称砝码.cpp
@@ -1,12 +1,12 @@
-#include <bitset>
 #include <iostream>
 #include <vector>
+#include <bitset>
 using namespace std;
-
 int main() {
     int n(0);
     while (cin >> n) {
-        vector<vector<int>> v(n, vector<int>(2, 0));
+        //input
+        vector<vector<int>> v(n, vector<int> (2, 0));
         v.reserve(20);
         for (int i = 0; i != n; i++) {
             cin >> v[i][0];
@@ -14,6 +14,7 @@ int main() {
         for (int i = 0; i != n; i++) {
             cin >> v[i][1];
         }
+        //v[i][0]weight,v[i][1]number
         int maxweight(0);
         for (int i = 0; i != n; i++) {
             maxweight += v[i][1] * v[i][0];
@@ -21,14 +22,18 @@ int main() {
         bitset<200001> b;
         bitset<200001> w;
         b.set(0);
+        //b.set(maxweight);
         for (int i = 0; i != n; i++) {
-            for (int j=0; j != v[i][1]; j++) {
+            for (int j = 0; j != v[i][1]; j++) {
                 w = b << v[i][0];
                 b = w | b;
                 w.reset();
+
             }
+
         }
         cout << b.count();
+
     }
+    return 0;
 }
-// 64 位输出请用 printf("%lld")
